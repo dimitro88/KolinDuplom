62dc96acdba7fb52a4f3024cfd82955d6d6553bb
dima <nevdaha505@gmail.com>
2018-12-09 21:17:11 +0200

    HI

--- repository/TaskRepository.js
+++ repository/TaskRepository.js
@@ -1,10 +1,16 @@
 const Task = require('../models/TaskModel');
+const Category = require('../models/CategoryModel');
+
+const brain = require('brain.js');
+
+const network = new brain.recurrent.LSTM();
 
 class TaskRepository {
 
   async createTask(taskBody) {
     const task = new Task(taskBody);
-    return await task.save();
+    await task.save();
+    return task;
   }
 
   async getTaskById(task_id) {
@@ -21,8 +27,37 @@ class TaskRepository {
 
 
   async createTaskByInformer(taskBody) {
-    const task = new Task(taskBody);
-    return await task.save();
+    try {
+      let tasks = await Task.find().populate(["categoryId"]);
+      console.log(tasks);
+      const trainingData = tasks.map(item => {
+        return {
+          input: item.task,
+          output: item.categoryId.name
+        };
+      });
+
+      network.train(trainingData, {
+        iterations: 50
+      });
+
+      const task = new Task(taskBody);
+      const output = network.run(task.task);
+      console.log(output);
+      let category = await Category.findOne({name: output});
+      task.answer = output;
+      task.categoryId = category._id;
+      await task.save();
+      let tasksForReturn = await Task.find({categoryId: category._id});
+      return {
+        category : output,
+        tasksForReturn};
+    }
+    catch(err){
+      return {
+        message : "Такої категорії не знайдено!"
+      }
+    }
   }
 
 
